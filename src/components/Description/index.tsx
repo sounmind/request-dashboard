3266a310ee8c5ac532f12e37c6a9dc926a87c82d
suhyeonglee <sounmind777@gmail.com>
2021-11-24 06:45:41 +0900

    feat(components): Description, Filter

---
+++ src/components/Description/index.tsx
@@ -0,0 +1,14 @@
+import React, { ReactNode } from "react";
+import styled from "styled-components";
+import FlexColumnBox from "../Layout/FlexColumnBox";
+
+const Wrapper = styled(FlexColumnBox)`
+  gap: 5px;
+  min-width: 300px;
+`;
+
+const Description: React.FC<{ children: ReactNode }> = ({ children }) => {
+  return <Wrapper>{children}</Wrapper>;
+};
+
+export default Description;
---
+++ src/components/Filter/index.tsx
@@ -0,0 +1,18 @@
+import React, { ReactNode } from "react";
+import styled from "styled-components";
+import FlexRowBox from "../Layout/FlexRowBox";
+
+const Wrapper = styled(FlexRowBox)`
+  justify-content: space-between;
+
+  @media only screen and (max-width: 375px) {
+    flex-direction: column;
+    align-items: start;
+  }
+`;
+
+const Filter: React.FC<{ children: ReactNode }> = ({ children }) => {
+  return <Wrapper>{children}</Wrapper>;
+};
+
+export default Filter;
